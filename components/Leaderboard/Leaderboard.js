e00ee8457681da079fce927fcd344f14826c6419
Mike Fortney <mike@inspired.com>
2021-12-10 23:42:51 -0800

    delete reference to deleted file...

--- components/Leaderboard/Leaderboard.js
+++ components/Leaderboard/Leaderboard.js
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import DataTable from "react-data-table-component";
-import daorace_styles from "../../styles/Leaderboard.module.css";
 import Breadcrumbs from "../Breadcrumbs";
 // A super simple expandable component.{JSON.stringify(data, null, 2)}
 const ExpandedComponent = ({ data }) => (
