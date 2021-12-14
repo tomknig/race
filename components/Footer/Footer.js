3e613af0c589115d3d99a795e70df20203268b79
Tom König <hi@tomknig.de>
2021-12-14 20:47:39 +0100

    Implement responsive footer
    https://trello.com/c/0zVJTPPe/25-footer-component-mobile

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -6,36 +6,45 @@ import Backslash from "../../public/backslash.svg";
 
 const Footer = () => {
   return (
-    <div className="w-full bg-[#FBFBFF] flex justify-center p-8 relative">
-      <div className="flex w-80 justify-evenly absolute left-10">
-        <a
-          href=""
-          className="rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-opacity-30"
-          rel="external noreferrer"
-          target=""
-        >
-          Home
-        </a>{" "}
-        <Image src={Backslash} height="" alt=""></Image>
-        <a
-          href="https://twitter.com/HyperscaleFund"
-          className="rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-opacity-30"
-          rel="external noreferrer"
-          target="_blank"
-        >
-          Twitter
-        </a>
-        <Image src={Backslash} height="" alt=""></Image>
-        <a
-          href="https://discord.com/invite/pVSbzYny2c"
-          className="rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-opacity-30"
-          rel="external noreferrer"
-          target="_blank"
-        >
-          Discord
-        </a>
+    <div className="w-full bg-[#FBFBFF] flex justify-center px-4 py-6 md:py-10">
+      <div className="w-screen max-w-7xl flex flex-wrap justify-center px-3">
+        <div className="flex justify-center md:justify-start grow basis-full md:basis-1">
+          <a
+            href=""
+            className="rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-opacity-30"
+            rel="external noreferrer"
+            target=""
+          >
+            Home
+          </a>{" "}
+          <div className="w-4 mx-4">
+            <Image src={Backslash} height="" alt=""></Image>
+          </div>
+          <a
+            href="https://twitter.com/HyperscaleFund"
+            className="rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-opacity-30"
+            rel="external noreferrer"
+            target="_blank"
+          >
+            Twitter
+          </a>
+          <div className="w-4 mx-4">
+            <Image src={Backslash} height="" alt=""></Image>
+          </div>
+          <a
+            href="https://discord.com/invite/pVSbzYny2c"
+            className="rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-opacity-30"
+            rel="external noreferrer"
+            target="_blank"
+          >
+            Discord
+          </a>
+        </div>
+        <div className="flex justify-center grow basis-full md:basis-1 mt-4 md:mt-0">
+          <Image src={Hyperscale} className="md:abso" width="95" alt=""></Image>
+        </div>
+        <div className="grow basis-0 md:basis-1" />
       </div>
-      <Image src={Hyperscale} width="120" alt=""></Image>
     </div>
   );
 };
