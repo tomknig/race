2ecc3448190962e705cb748e837117829d64e833
Mike Fortney <mike@inspired.com>
2021-12-14 14:06:40 -0800

    moved footer from individual pages to layout component (except for homepage which is a one-off), added links to expanded leaderboard row, added background to header

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Header from "../Header/Header";
+import Footer from "../../components/Footer";
 
 export default function Layout({ title = "Hyperscale", children }) {
   const pageTitle = title !== "Hyperscale" ? `Hyperscale - ${title}` : title;
@@ -9,11 +10,25 @@ export default function Layout({ title = "Hyperscale", children }) {
         <title>{pageTitle}</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <div className="flex justify-center">
-        <div className="w-screen max-w-7xl px-4 xl:px-0">
-          <Header />
-          {children}
+      <div
+        style={{
+          backgroundImage: "url('/img/navigation-background.svg')",
+          backgroundSize: "cover",
+          backgroundPosition: "center center",
+          backgroundRepeat: "no-repeat",
+        }}
+      >
+        <div className="flex justify-center">
+          <div className="w-screen max-w-7xl px-4 xl:px-0">
+            <Header />
+          </div>
         </div>
+      </div>
+      <div className="flex justify-center">
+        <div className="w-screen max-w-7xl px-4 xl:px-0">{children}</div>
+      </div>
+      <div className="mt-12">
+        <Footer />
       </div>
     </>
   );
